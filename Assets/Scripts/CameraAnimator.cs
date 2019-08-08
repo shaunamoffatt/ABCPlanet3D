cbf020e2a4c9a4edeb7bf1341d5c21bb980894a7
Shauna Moffatt <shaunamoffatt@gmail.com>
2019-08-08 16:57:48 +0100

    refactor cameraAnimator- not need for update, using delegate handler instead

--- Assets/Scripts/CameraAnimator.cs
+++ Assets/Scripts/CameraAnimator.cs
@@ -6,11 +6,9 @@ using UnityEngine;
 
 public class CameraAnimator : MonoBehaviour
 {
-    private Vector3 position;
     private Animator anim;
-    
-    GameManager GM;
-    GameState animatorState;
+    private GameManager GM;
+
     // Start is called before the first frame update
 
     void Awake()
@@ -22,38 +20,26 @@ public class CameraAnimator : MonoBehaviour
     public void HandleOnStateChange()
     {
         Debug.Log("CamerAnimator on state change!");
+        if(GM.previousState != GM.gameState)
+            ChangeAnimation();
     }
 
 
     void Start()
     {
-
         GM.SetGameState(GameState.MAIN);
-        animatorState = GameState.MAIN;
         anim = GetComponent<Animator>();
 
     }
     
-    void Update()
-    {
-        //if the gamestate has changed but not updated the cameraAnimator..
-        //...animator state. Time to animate.
-        //if(GM.previousState != null)
-        if (GM.gameState != animatorState)
-        {
-            Debug.Log("Change animation");
-            ChangeAnimation();
-        }
-    }
-
     /// <summary>
     /// Animation Triggers on the camera are named so that it show what transition state has just happened
     /// For eg. if going from the main view to sun view the trigger is named "mainsun"
     /// going from the sun view to the main is named "sunmain" etc.
     /// </summary>
-    private void ChangeAnimation()
+    public void ChangeAnimation()
     {
-        animatorState = GM.gameState;
+        //TODO delete
         Debug.Log(GM.previousState.ToString().ToLower() + GM.gameState.ToString().ToLower());
         anim.SetTrigger(GM.previousState.ToString().ToLower() + GM.gameState.ToString().ToLower());
     }
